e1b2a39726ac274f4037b0595408520a8c3e105a
Kat B <katreesebarnes@gmail.com>
2019-04-11 13:28:03 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,10 @@
 class BankAccount
-attr_accessor :name, :balance, :open
+attr_accessor :name, :balance, :status
 
 def initialize(name)
   @name = name 
   @balance = 1000
-  @open = open
+  @status = open
 end
 
 end
